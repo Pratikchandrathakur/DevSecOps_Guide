adeeaa1fbcccef56dca81e2589f08dbfd2600f45
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2026-09-06 14:00:01 +0545

    Add variables for ASG module configuration

---
+++ AWS_DevSecOps/v3/infra/modules/asg/variables.tf
@@ -0,0 +1,9 @@
+variable "name" { type = string }
+variable "app_subnet_ids" { type = list(string) }
+variable "app_sg_id" { type = string }
+variable "target_group_arn" { type = string }
+variable "instance_type" { type = string }
+variable "desired_capacity" { type = number }
+variable "min_size" { type = number }
+variable "max_size" { type = number }
+variable "ami_id" { type = string } # CHANGE_ME if not using data source
